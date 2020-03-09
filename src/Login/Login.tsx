612b2caed11712603e584e131e5d394f95ad9ba4
Brad Ringel <brad.ringel@gmail.com>
2020-03-09 18:29:30 -0400

    set up login page w/ email password and some oauth options

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 
+import facebook from '../images/facebook_white.svg';
+import google from '../images/google_white.svg';
+import microsoft from '../images/microsoft_white.svg';
+
 type Props = {};
 
 const Login = (props: Props) => {
   return (
-    <div className="relative">
-      <div className="bg-white rounded-sm m-3  md:w-1/2 lg:w-1/4 shadow-md border md:absolute md:top-0 md:right-0 md:mt-4 md:mr-4 px-4 py-6">
-        <button className="bg-green-600 py-2 px-4 rounded-md text-white w-full text-lg">Log In</button>
-        <div className="text-xl flex justify-center my-1">&mdash; or &mdash;</div>
-        <button className="bg-green-600 py-2 px-4 rounded-md text-white w-full text-lg">Sign Up</button>
+    <div className="flex justify-center p-2">
+      <div className="bg-white rounded-sm m-3 px-4 py-6 border shadow-md flex-none w-full md:w-1/2 lg:w-1/4 ">
+        <label htmlFor="email">Email</label>
+        <input name="email" className="input w-full mb-1" />
+        <label htmlFor="password">Password</label>
+        <input name="password" type="password" className="input w-full mb-1" />
+        <button className="btn bg-green-600 text-white w-full hover:bg-green-700 my-2">Log In</button>
+        <div>
+          Don't have an account?&nbsp;
+          <a href="/signup" className="text-green-600 underline">
+            Sign Up
+          </a>
+        </div>
+        <button className="btn mt-2 bg-facebook hover:bg-facebook-darker text-white w-full flex align-center px-10">
+          <img src={facebook} width={24} height={24} className="mr-4" alt="facebook logo" />
+          Sign in with Facebook
+        </button>
+        <button className="btn mt-2 bg-google hover:bg-google-darker text-white w-full flex align-center px-10">
+          <img src={google} width={24} height={24} className="mr-4" alt="google logo" />
+          Sign in with Google
+        </button>
+        <button className="btn mt-2 bg-microsoft hover:bg-microsoft-darker text-white w-full flex align-center px-10">
+          <img src={microsoft} width={24} height={24} className="mr-4" alt="microsoft logo" />
+          Sign in with Microsoft
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        facebook: {
+          default: '#1877F2',
+          darker: '#166BDA'
+        },
+        google: {
+          default: '#DB4437',
+          darker: '#C53D32'
+        },
+        microsoft: {
+          default: '#666666',
+          darker: '#5C5C5C'
+        }
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
