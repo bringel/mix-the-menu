774eb47ef6c55d6b6a0e73f048a2b66b31900607
Brad Ringel <brad.ringel@gmail.com>
2020-10-11 14:53:46 -0400

    A few nav color tweaks

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -10,9 +10,9 @@ const Layout = (props: Props) => {
   const { isSignedIn } = useAuthContext();
   return (
     <div className="min-h-screen bg-gray-50">
-      <nav className="flex justify-between mb-2 py-2 px-8 bg-primary-500 text-lg text-white">
+      <nav className="flex justify-between mb-2 py-2 px-8 bg-primary-500 text-lg text-grey-800">
         <div className="flex items-center">
-          <h1 className="text-3xl font-header font-bold text-white mr-6">Mix the Menu</h1>
+          <h1 className="text-3xl font-header font-bold mr-6">Mix the Menu</h1>
           {isSignedIn && (
             <div className="mt-2">
               <NavLink to="/dashboard" className="mr-2 py-1 px-3" activeClassName="bg-primary-600 rounded">
@@ -26,8 +26,8 @@ const Layout = (props: Props) => {
         </div>
         {isSignedIn && (
           <div className="flex items-center mt-2">
-            <Link to="/create" className="bg-white text-primary-500 font-semibold rounded mr-2 py-2 px-4">
-              Create
+            <Link to="/create" className="bg-gray-800 text-white font-semibold rounded mr-2 py-2 px-4">
+              Create Menu
             </Link>
             <NavLink to="/user/settings" className="mr-2 py-1 px-3" activeClassName="bg-primary-600 rounded">
               Settings
